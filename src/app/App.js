9b3a94ae0b2646b77ab264d7951ee6df592df0f9
ayalbir <ayal.birenstock@live.biu.ac.il>
2024-06-03 15:37:18 +0300

    adding links to the elements, adding logo. the main page (the base of it) is done.

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import LeftMenu from '../leftMenu/LeftMenu';
 import videosData from '../videoItem/Videos';
 import Search from '../search/Search';
 import VideoListResults from '../videoListResults/VideoListResults';
-import items from '../leftMenu/menuItems/MenuItems.json';
 import VideoDetails from '../videoDetails/VideoDetails';
 import SearchResults from '../search/SearchResults'; // Import the new component
 
@@ -21,18 +20,23 @@ function App() {
     <Router>
       <div className="container-fluid">
         <div className="row">
-          <LeftMenu menuItems={items} expanded={expanded} setExpanded={setExpanded} />
+          <LeftMenu expanded={expanded} setExpanded={setExpanded} />
           <div className={`col main-content ${expanded ? 'offset-md-3' : 'offset-md-1'}`}>
+            <div className="search-signin-container">
+            <Link to="/" className="app-logo-link">
+            <img src="/images/logo.svg" alt="Logo" className="app-logo" />
+            </Link>
+
+              <Search />
+              <Link to ="/signin" className="signin-link">
+              <button type="button" className="btn btn-primary">
+                <span className="bi bi-person-circle" aria-hidden="true"></span>
+                <span className="signin-text">Sign in</span>
+              </button>
+              </Link>
+            </div>
             <Routes>
-              <Route path="/" element={
-                <>
-                  <div className="search-signin-container">
-                    <Search />
-                    <button type="button" className="btn btn-primary">Sign in</button>
-                  </div>
-                  <VideoListResults videos={videoList} />
-                </>
-              } />
+              <Route path="/" element={<VideoListResults videos={videoList} />} />
               <Route path="/video/:id" element={<VideoDetails />} />
               <Route path="/search" element={<SearchResults videos={videoList} />} /> {/* New route */}
             </Routes>
