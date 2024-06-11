5862cc20bfb61f280b5314d5bea1a3be942c5b69
ayalbir <ayal.birenstock@live.biu.ac.il>
2024-06-11 14:23:58 +0300

    just to save that the website is displaying

--- src/loginSignin/SignIn.js
+++ src/loginSignin/SignIn.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './signIn.css';
+import './SignIn.css';
 
 function SignIn() {
   const [email, setEmail] = useState('');
---
+++ src/loginSignin/SignInLayout.js
@@ -0,0 +1,13 @@
+// SignInLayout.js
+
+import React from 'react';
+
+function SignInLayout({ children }) {
+  return (
+    <div className="sign-in-layout">
+      {children}
+    </div>
+  );
+}
+
+export default SignInLayout;
