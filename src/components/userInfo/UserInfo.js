c00ae9f258cc0ba5489e9ed8c98bb9d11c40d1bf
DvirYaz27 <dviry27@gmail.com>
2024-09-02 20:38:18 +0300

    patching upper prof img

--- src/components/userInfo/UserInfo.js
+++ src/components/userInfo/UserInfo.js
@@ -5,12 +5,26 @@ import { Link } from 'react-router-dom';
 import './UserInfo.css';
 
 const UserInfo = ({ user, handleSignOut }) => {
+  // Function to ensure the profile image URL is correctly formatted
+  const formatProfileImage = (image) => {
+    if (image && !image.startsWith('data:image/')) {
+      return `data:image/jpeg;base64,${image}`; // Adjust 'image/jpeg' if you use different image formats
+    }
+    return image;
+  };
+
   return (
     <div className="user-info">
       {user ? (
         <div>
           <Link to="/update-profile" className="update-profile-link">
-            {user.profileImage && <img src={user.profileImage} alt="Profile" className="profile-image" />}
+            {user.profileImage && (
+              <img
+                src={formatProfileImage(user.profileImage)} // Ensure the image URL is correctly formatted
+                alt="Profile"
+                className="profile-image"
+              />
+            )}
             <span className="user-details">{user.firstName}</span>
           </Link>
           <Link to="/signin" className="signin-link">
